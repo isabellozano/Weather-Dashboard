bffede9fa09f0dfc6db98435e07dcb5918aab20c
isa <54730392+isabellozano@users.noreply.github.com>
2019-11-09 22:26:03 -0800

    Updated Weather Dashboard

---
+++ index.js
@@ -0,0 +1,9 @@
+// So this is where I would have to use AJAX for my functions to actually work.
+
+// I'd have my function for my search bar right here where it'd be dependent of an on click function from the search button attached to it which would be to the right side of it, childed to the search textbox and positioned to the right of it (that's more CSS stuff for looks).
+
+// I'd have a dialog box function right here so that it is fixed, but the information is not upon starting, however it would stay fixed and locally stored upon the first request / search for a city's weather forecast.
+
+// Upon the first search of a city's weather and the search button function being called (once that first set of weather data comes up) I'd have a pop up modal that would pop up, but below the first fixed dialog box/modal and towards the bottom of the page / screen, with the 5 day weather forecast of that city that the user was inquiring about. The data would be locally stored and stay if the user refreshed the page and would only be cleared if the user entered a new city.
+
+// I'd have a function that saves the users recent searches (also be locally stored) by order of search in the search bar in some kind of dropdown box.
